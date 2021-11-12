f2cf8945bd7174d17df90281e3934096a9ab413d
Ajay Ghori <ajay.ghori.5@gmail.com>
2021-11-12 21:07:29 +0530

    hi

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+app.listen(process.env.PORT || port, ()=>{console.log(`Listnening at ${port}`)});
+
+app.use(express.static('public'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname , '/public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err);
+      console.log("Something wrong");
+    }
+  })
+})
